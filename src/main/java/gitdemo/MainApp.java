290eb24a7ab699d1e7597f81d6aa9cf98824ba7b
John Loane <john.loane@dkit.ie>
2020-03-24 14:52:27 +0000

    Add Utility with printMessage
    This demos adding a new file and modifiying an existing file

--- src/main/java/gitdemo/MainApp.java
+++ src/main/java/gitdemo/MainApp.java
@@ -1,5 +1,5 @@
 package gitdemo;
-
+//Git demo example
 public class MainApp
 {
     public static void main(String[] args)
---
+++ src/main/java/gitdemo/Utility.java
@@ -0,0 +1,9 @@
+package gitdemo;
+
+public class Utility
+{
+    public static void printMessage(String messageToPrint)
+    {
+        System.out.println(messageToPrint);
+    }
+}
